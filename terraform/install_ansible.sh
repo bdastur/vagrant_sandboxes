274c46a98b48224fb7c148e1d2da852c08adb242
bdastur <bdastur@gmail.com>
2016-05-12 17:23:14 -0700

    Add a working tf file, bootstrap and install ansible

---
+++ terraform/install_ansible.sh
@@ -0,0 +1,20 @@
+VIRTUALENV="/home/vagrant/ansible_env"
+
+# Install dependencies, including virtualenv.
+sudo yum install -y git \
+        gcc \
+            wget \
+                libffi libffi-devel openssl-devel \
+                    python-virtualenv
+
+# Install sshpass
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+sudo rpm -ivh epel-release-7-6.noarch.rpm
+sudo yum install -y sshpass
+
+virtualenv ${VIRTUALENV}
+source ${VIRTUALENV}/bin/activate
+pip install ansible==1.9.4
+ 
+echo "Done!"
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+instance_type = "t2.medium"
+ami = "ami-22222222"
+
